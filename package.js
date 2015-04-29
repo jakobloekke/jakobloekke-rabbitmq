63ce6336e23a8acf6a3959cbb27e8f8010ddefa4
Jakob Løkke Madsen <jakob@jakobloekkemadsen.com>
2015-04-29 12:31:05 +0200

    First commit

---
+++ package.js
@@ -0,0 +1,27 @@
+Package.describe({
+    name: 'jakobloekke:rabbitmq',
+    version: '0.0.1',
+    // Brief, one-line summary of the package.
+    summary: 'node-amqp wrapped as an EventEmitter',
+    // URL to the Git repository containing the source code for this package.
+    git: '',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
+});
+
+Npm.depends({
+    "amqp": "0.2.4"
+});
+
+Package.onUse(function (api) {
+    api.versionsFrom('1.1.0.2');
+    api.addFiles('rabbitmq.js', 'server');
+    api.export('RabbitMQ', 'server');
+});
+
+Package.onTest(function (api) {
+    api.use('tinytest');
+    api.use('jakobloekke:rabbitmq');
+    api.addFiles('rabbitmq-tests.js');
+});
---
+++ rabbitmq.js
@@ -0,0 +1,47 @@
+var amqp = Npm.require('amqp'),
+    EventEmitter = Npm.require('events').EventEmitter;
+
+RabbitMQ = _objectAsEventEmitter({});
+RabbitMQ.connection = null;
+RabbitMQ.exchanges = {};
+
+RabbitMQ.createConnection = function (options) {
+    RabbitMQ.connection = amqp.createConnection(options);
+
+    RabbitMQ.connection.on('error', function (err) {
+        throw new Meteor.Error(err);
+    });
+};
+
+
+/**
+ * http://stackoverflow.com/a/29722360/245540
+ * @param obj
+ */
+function _objectAsEventEmitter(obj) {
+    // copy EventEmitter prototype to obj, but make properties
+    // non-enumerable
+    for (var prop in EventEmitter.prototype) {
+        Object.defineProperty(obj, prop, {
+            configurable: true,
+            writable: true,
+            value: EventEmitter.prototype[prop]
+        });
+    }
+
+    // also, make sure the following properties are hidden
+    // before the constructor adds them
+    ["domain", "_events", "_maxListeners"].forEach(function (prop) {
+        Object.defineProperty(obj, prop, {
+            configurable: true,
+            writable: true,
+            value: undefined
+        });
+    });
+
+    // call EventEmitter constructor on obj
+    EventEmitter.call(obj);
+
+    // return the obj, which should now function as EventEmitter
+    return obj;
+}
