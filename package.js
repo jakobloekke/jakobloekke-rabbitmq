a81499ae2ab6701bc8b93ff47ea9f169adaef2dc
Jakob Løkke Madsen <jakob@jakobloekkemadsen.com>
2015-04-29 12:54:51 +0200

    Version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'jakobloekke:rabbitmq',
-    version: '0.0.1',
+    version: '0.0.2',
     // Brief, one-line summary of the package.
     summary: 'node-amqp wrapped as an EventEmitter',
     // URL to the Git repository containing the source code for this package.
